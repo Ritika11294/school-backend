04fcad1eff6da91db34b96bb157bfa327a90d0c6
Ritika <ritikagupta11294@gmail.com>
2022-04-17 23:45:05 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const teacherController = require("./controllers/teacher.controller")
 const classController = require('./controllers/class.controller')
 // const adminController = require("./controllers/admin.controller")
 const cors = require("cors");
-const port = process.env.PORT || 2455;
+// const port = process.env.PORT || 2455;
 
 
 
@@ -22,11 +22,16 @@ app.use("/teachers", teacherController)
 app.use('/classes', classController)
 // app.use('/admin', adminController)
 
-app.listen(2455, async function () {
-    try {
-        await connect()
-        console.log(`listening on port ${port}`)
-    } catch (error) {
-        console.log('error:', error)
-    }
-})
+// app.listen(2455, async function () {
+//     try {
+//         await connect()
+//         console.log(`listening on port ${port}`)
+//     } catch (error) {
+//         console.log('error:', error)
+//     }
+// })
+
+const server = app.listen(process.env.PORT || 5000, () => {
+    const port = server.address().port;
+    console.log(`Express is working on port ${port}`);
+});
