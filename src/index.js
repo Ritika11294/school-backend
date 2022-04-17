2f42b7779a6fdc31d85ebac1fa75a73771544282
Ritika <ritikagupta11294@gmail.com>
2022-04-17 23:01:17 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ const connect = require("./configs/db")
 const teacherController = require("./controllers/teacher.controller")
 const classController = require('./controllers/class.controller')
 // const adminController = require("./controllers/admin.controller")
+const cors = require("cors");
+const port = process.env.PORT || 2455;
+
 
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
-const {register, login} = require("./controllers/auth.controller");
-app.post("/register", register); 
-app.post("/login", login);  
+const { register, login } = require("./controllers/auth.controller");
+app.post("/register", register);
+app.post("/login", login);
 
 
 app.use("/teachers", teacherController)
@@ -21,7 +25,7 @@ app.use('/classes', classController)
 app.listen(2455, async function () {
     try {
         await connect()
-        console.log('listening on port 2455')
+        console.log(`listening on port ${port}`)
     } catch (error) {
         console.log('error:', error)
     }
